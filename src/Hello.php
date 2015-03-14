43b8469f9451a7bb8e8e9f807c88f532d0d37caa
rtutin <qqk8@ya.ru>
2015-03-14 14:54:27 +0300

    Update Hello.php

--- src/Hello.php
+++ src/Hello.php
@@ -1,4 +1,4 @@
 <?php
-function hello ($name) {
-    return 'Hello, '.$name.'!';
+function hello () {
+    if (isset(func_get_arg(0)) { return 'Hello, '.func_get_arg(0).'!'; } else { return 'Hello, World!'; }
 }
