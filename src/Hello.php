bfb132d904c30fb84acc59eb9e0c91c1d72f7381
rtutin <qqk8@ya.ru>
2015-03-14 14:46:09 +0300

    Update Hello.php

--- src/Hello.php
+++ src/Hello.php
@@ -1,25 +1,4 @@
 <?php
-namespace Rivsen\Demo;
-
-class Hello
-{
-    private $name;
-
-    public function __construct( $name = 'World' )
-    {
-        $this->name = $name;
-    }
-
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function hello()
-    {
-        return 'Hello '.$this->name.'!';
-    }
-}
 function hello ($name) {
     return 'Hello, '.$name.'!';
 }
